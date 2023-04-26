aa3d5a05e19b0624bf85999e830b03c2550f620d
gbenites <gracielabenitesf@gmail.com>
2023-04-25 23:11:52 -0500

    test edit pokemon

--- src/app/pages/list-pokemons/edit-pokemon/edit-pokemon.component.spec.ts
+++ src/app/pages/list-pokemons/edit-pokemon/edit-pokemon.component.spec.ts
@@ -1,11 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { PokemonService } from 'src/app/service/pokemon.service';
+import { getAllPokemons, onePokemonDataTest } from 'src/app/service/pokemon.service.mocks';
 
 import { EditPokemonComponent } from './edit-pokemon.component';
 
 describe('EditPokemonComponent', () => {
   let component: EditPokemonComponent;
   let fixture: ComponentFixture<EditPokemonComponent>;
+  let pokemonService: PokemonService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,9 +21,24 @@ describe('EditPokemonComponent', () => {
     fixture = TestBed.createComponent(EditPokemonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    pokemonService = TestBed.inject(PokemonService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should edit pokemon', () => {
+
+    // Arrange
+    const editPokemonSpy = spyOn(pokemonService, 'updatePokemon').and.returnValue(
+      of(getAllPokemons)
+    );
+
+    // Act
+    component.updatePokemon(onePokemonDataTest);
+
+    // Assert
+    expect(editPokemonSpy).toHaveBeenCalled();
+  })
 });
