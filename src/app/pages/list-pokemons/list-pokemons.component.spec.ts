c53ffcb9ad599cbb71241681d1f6c0a0352a5de9
gbenites <gracielabenitesf@gmail.com>
2023-04-25 22:37:24 -0500

    test

--- src/app/pages/list-pokemons/list-pokemons.component.spec.ts
+++ src/app/pages/list-pokemons/list-pokemons.component.spec.ts
@@ -1,11 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
+import { PokemonService } from 'src/app/service/pokemon.service';
 
 import { ListPokemonsComponent } from './list-pokemons.component';
 
 describe('ListPokemonComponent', () => {
   let component: ListPokemonsComponent;
   let fixture: ComponentFixture<ListPokemonsComponent>;
+  let pokemonService: PokemonService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,7 +21,7 @@ describe('ListPokemonComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
 
-    component.addPokemon = false;
+    pokemonService = TestBed.inject(PokemonService);
   });
 
   it('should create', () => {
@@ -26,6 +29,24 @@ describe('ListPokemonComponent', () => {
   });
 
   it('dsfdsfsd', () => {
+    const stream = of(
+[{
+      id: 35,
+      name: "Alakazam",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/065.png",
+      attack: 79,
+      defense: 61,
+      hp: 55,
+      type: "Eléctrico",
+      idAuthor: 1,
+      }]
+      );
+    const spyon2 = spyOn(pokemonService, 'getAllPokemons').and.returnValue(stream);
+console.log('----spyon2', spyon2);
+
+component.ngOnInit();
+    expect(spyon2).toHaveBeenCalled();
+
     console.log('component', component.addPokemon);
     
   })
